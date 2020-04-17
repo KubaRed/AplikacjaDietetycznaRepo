043f5df19db5ed5fe3b8c40817625b1bce942dd5
Pajfu <pajfot@gmail.com>
2020-04-17 19:08:05 +0200

    Dodanie buttonów Zaloguj i Rejestracja

--- AplikacjaDietetyczna/AplikacjaDietetyczna/MainWindow.xaml.cs
+++ AplikacjaDietetyczna/AplikacjaDietetyczna/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace AplikacjaDietetyczna
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+            
+        }
     }
 }
